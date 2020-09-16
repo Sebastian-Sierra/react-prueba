c4f8456b90ff9a41535e909f088e7b60dd9a96ad
Sebastian-Sierra <ysebasst@gmail.com>
2020-09-16 05:42:00 +0000

    cambio en la indentacion

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="content" id="fecha_hora">
-    <div id="dia_semana">Dia semana</div>
-    <div id="fecha">Fecha</div>
-    <div id="hora">Hora</div>
-  </div>
+      <div id="dia_semana">Dia semana</div>
+      <div id="fecha">Fecha</div>
+      <div id="hora">Hora</div>
+    </div>
   );
 }
 
